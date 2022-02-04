e0e228dc1c29ca521f25600df383f3bb76967b8c
Rick <rickysan98@gmail.com>
2022-02-04 17:09:15 +0100

    Fix torretta

--- Assets/Scripts/Torretta.cs
+++ Assets/Scripts/Torretta.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Torretta : MonoBehaviour
 {
+    //player
     public Transform target;
     public GameObject parteDaRuotare;
+    // distanza dall'area di attacco della torretta
+    public float attackarea;
 
     void Update()
     {
+        // se la distanza tra il player e la torretta supera la distanza di attacco allora la torretta comincia a ruotare individuando la posizione del player
+        if (Vector3.Distance(transform.position, target.position) < attackarea)
+        {
+            parteDaRuotare.transform.LookAt(target);
+            print(target.position);
+        }
         
-        parteDaRuotare.transform.LookAt(target);
-        print(target.position);
 
     }
 
