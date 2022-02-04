cdbf06f23c0294e6dff12957ef1fdfabf64a336e
Rick <rickysan98@gmail.com>
2022-02-04 17:44:24 +0100

    Fix

--- Assets/Scripts/Torretta.cs
+++ Assets/Scripts/Torretta.cs
@@ -9,14 +9,23 @@ public class Torretta : MonoBehaviour
     public GameObject parteDaRuotare;
     // distanza dall'area di attacco della torretta
     public float attackarea;
+    Vector3 rotazioneIniziale;
 
+    private void Start()
+    {
+        rotazioneIniziale = new Vector3(0, 180, 0);
+    }
     void Update()
     {
         // se la distanza tra il player e la torretta supera la distanza di attacco allora la torretta comincia a ruotare individuando la posizione del player
         if (Vector3.Distance(transform.position, target.position) < attackarea)
         {
             parteDaRuotare.transform.LookAt(target);
-            print(target.position);
+            
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(rotazioneIniziale);
         }
         
 
