8b6f6a418ae7838c987e387b7b2aa0ce4e7b8736
Rick <rickysan98@gmail.com>
2022-02-07 23:50:33 +0100

    commento codice

--- Assets/Scripts/Proiettile.cs
+++ Assets/Scripts/Proiettile.cs
@@ -13,17 +13,18 @@ public class Proiettile : MonoBehaviour
     
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;//altrimenti i proiettili andrebbero in un'unica direzione
 
-        target = new Vector3(player.position.x, player.position.y, player.position.z);
+        target = new Vector3(player.position.x, player.position.y, player.position.z);// target = posizione del player
     }
 
     
     void Update()
     {
         
-        
+        // il p. si muove verso il player
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        //se le coordinate del proiettile sono uguali a quelle del player, allora il proiettile si degenera
             if (transform.position.x == target.x && transform.position.y == target.y && transform.position.z == target.z)
             {
                 Distruggiproiettile();
@@ -31,7 +32,7 @@ public class Proiettile : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)// se collide con il player allora si degenera
     {
         if (other.CompareTag("Player"))
         {
@@ -39,7 +40,7 @@ public class Proiettile : MonoBehaviour
         }
     }
 
-    void Distruggiproiettile()
+    void Distruggiproiettile()//distruggo il proiettile
     {
         Destroy(gameObject);
     }
--- Assets/Scripts/Torretta.cs
+++ Assets/Scripts/Torretta.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Torretta : MonoBehaviour
 {
-    //player
-    public Transform target;
-    public GameObject parteDaRuotare;
-    // distanza dall'area di attacco della torretta
+                                                             
+    public Transform target;//target sarebbe il player 
+    public GameObject parteDaRuotare;// distanza dall'area di attacco della torretta
     public float attackarea;
     Vector3 rotazioneIniziale;
 
     private float timebeetweenshots;
-    public float starttimebetweenshots;
+    public float starttimebetweenshots;// e' il tempo tra un proiettile e l'altro
     public GameObject meshproiettile;
 
     private void Start()
@@ -22,17 +21,21 @@ public class Torretta : MonoBehaviour
     }
     void Update()
     {
-        // se la distanza tra il player e la torretta supera la distanza di attacco allora la torretta comincia a ruotare individuando la posizione del player
+        // se la distanza tra il player e la torretta supera la distanza di attacco allora la torretta comincia a ruotare e sparare  individuando la posizione del player
         if (Vector3.Distance(transform.position, target.position) < attackarea)
         {
             parteDaRuotare.transform.LookAt(target);
+
+            // se il tempo tra uno sparo e l'aktro e' minore o uguale a zero allora la torreta genera un proiettile
             if (timebeetweenshots <= 0)
             {
+                //istanzio quello che devo generare( ovvero il proiettile), la sua posizione e rotazione nello spazio(in questo caso non ruota)
                 Instantiate(meshproiettile, transform.position, Quaternion.identity);
-                timebeetweenshots = starttimebetweenshots;
+                timebeetweenshots = starttimebetweenshots;// se non faccio cosi, la torretta sparerebbe un proiettile ad ogni frame
             }
-            else
+            else 
             {
+                
                 timebeetweenshots -= Time.deltaTime;
             }
 
