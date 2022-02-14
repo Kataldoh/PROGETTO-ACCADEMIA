c85bce8f0da0f7580a62891165c2fe8312a1fc80
RiccardoSantochi <rickysan98@gmail.com>
2022-02-14 17:03:15 +0100

    enemycrawl

--- Assets/Scripts/enemyCRAWL.cs
+++ Assets/Scripts/enemyCRAWL.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class enemyCRAWL : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movespeed;
+    public GameObject[] nodi;
+
+    int nodosuccessivo = 1;
+    float distanzanodi;
+
+     void Update()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+     void Move()
     {
-        
+        distanzanodi = Vector3.Distance(transform.position, nodi[nodosuccessivo].transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, nodi[nodosuccessivo].transform.position,
+            movespeed * Time.deltaTime);
+
+        if (distanzanodi < 0.2f)
+        {
+            TakeTurn();
+        }
+
+        void TakeTurn()
+        {
+            Vector3 rotation = transform.eulerAngles;
+            rotation.x = nodi[nodosuccessivo].transform.eulerAngles.x;
+            transform.eulerAngles = rotation;
+
+            NextNodo();
+
+        }
+
+        void NextNodo()
+        {
+            nodosuccessivo++;
+
+            if (nodosuccessivo == nodi.Length)
+            {
+                nodosuccessivo = 0;
+            }
+        }
     }
 }
