9648b52a6d9d82d2a565730b9522034e1bdd0177
Chiara <62606760+TachifluDec@users.noreply.github.com>
2022-03-09 12:52:47 +0100

    catena montaggio

--- Assets/CatenaMontaggio.cs
+++ Assets/CatenaMontaggio.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CatenaMontaggio : MonoBehaviour
 {
+    public Transform fineCatena;
+    public Transform inizioCatena;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
         
     }
+    
+
+
+
+    void OnTriggerEnter()
+    {
+
+    }
+    
 }
