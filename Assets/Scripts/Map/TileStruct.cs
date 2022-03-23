5979b93e08e91be147dea8ccaf83359bdf5d279c
EliasLevin <pavanelloelia1@gmail.com>
2022-03-23 12:53:21 +0100

    Update MapEditor

---
+++ Assets/Scripts/Map/TileStruct.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class TileStruct
+{
+    public GameObject tile;
+    public int dimx = 1;
+    public int dimy = 1;
+    public bool start;
+    public bool end;
+}
