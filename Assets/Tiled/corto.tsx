2237fe3a2d00cf18e72ec32aea9438f854e8ae2d
Federico <79253087+FedericoBondi@users.noreply.github.com>
2022-04-07 02:50:08 +0200

    Mappa Aggiornata

---
+++ Assets/Tiled/corto.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="corto" tilewidth="16" tileheight="16" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="Platformer - desert/Tileset/bridge1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="Platformer - desert/Tileset/bridge3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="Platformer - desert/Tileset/grass10.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="Platformer - desert/Tileset/platvariation3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="Platformer - desert/Tileset/platvariation6.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain20.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain23.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="Platformer - desert/Tileset/water1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="Platformer - desert/Tileset/grass2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain5.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="Platformer - desert/Tileset/water2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain14.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain15.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="Platformer - desert/Tileset/tileMain19.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="Platformer - desert/Tileset/bridge6.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="Platformer - desert/Tileset/platvariation16.png"/>
+ </tile>
+</tileset>
