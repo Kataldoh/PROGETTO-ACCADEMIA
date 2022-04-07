490c7b6feb69bfba02594bd38578e25c7accee05
Kataldoh <69595625+Kataldoh@users.noreply.github.com>
2022-04-07 12:18:12 +0200

    Update SoundManager (pooling delle AudioSource)

--- Assets/Scripts/AudioScripts/SoundManager.cs
+++ Assets/Scripts/AudioScripts/SoundManager.cs
@@ -19,21 +19,44 @@ public static class SoundManager
         PowerUp,
     }
 
-    public static void PlaySound( Sound sound)
+    private static Dictionary<Sound, float> soundTimerDictionary; //Contiene i timer
+    private static GameObject oneShotGameObject;
+    private static AudioSource oneShotAudioSource;
+
+    public static void PlaySound(Sound sound,Vector3 position)
     {
 
         if (CanPlaySound(sound))
         {
             GameObject soundGameObject = new GameObject("Sound");
+            soundGameObject.transform.position = position;
             soundGameObject.AddComponent<AudioSource>();
             AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
-            audioSource.PlayOneShot(GetAudioClip(sound));
+            audioSource.clip = GetAudioClip(sound);
+            audioSource.Play();
+        }
+
+
+    }
+
+    public static void PlaySound( Sound sound)
+    {
+
+        if (CanPlaySound(sound))
+        {
+
+            if (oneShotGameObject == null)
+            {
+                oneShotGameObject= new GameObject("Sound");
+                oneShotAudioSource = oneShotGameObject.AddComponent<AudioSource>();
+
+            }
+            oneShotAudioSource.PlayOneShot(GetAudioClip(sound));
         }
 
     
     }
 
-    private static Dictionary<Sound, float> soundTimerDictionary;
 
     public static void Initialize()
     {
@@ -66,11 +89,11 @@ public static class SoundManager
                 if (soundTimerDictionary.ContainsKey(sound))
                 {
                     float lastTimePlayed = soundTimerDictionary[sound];
-                    float MusicTimerMax = 167f;
+                    float MusicTimerMax = 167f; //Valore che determina quanto spesso il sound viene riprodotto
 
-                    if(lastTimePlayed + MusicTimerMax < Time.time)
+                    if(lastTimePlayed + MusicTimerMax < Time.time) //Se il tempo trascorso dall'ultima volta che il suono è stato riprodotto + il valore MusicTimerMax è minore di Time.time
                     {
-                        soundTimerDictionary[sound] = Time.time;
+                        soundTimerDictionary[sound] = Time.time;// si può far ripartire il sound
                         return true;
                     }
                     else
