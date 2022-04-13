fa5d4e2e87526360ffb9e52e609039921876f98f
Fragalax <79252569+Fragalax@users.noreply.github.com>
2022-04-13 12:53:08 +0200

    Text Menu Updated

--- Assets/Scripts/UIScripts/ButtonsExtraScript.cs
+++ Assets/Scripts/UIScripts/ButtonsExtraScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using TMPro;
@@ -8,23 +9,31 @@ using TMPro;
 public class ButtonsExtraScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler//, ISelectHandler
 {
     //TMPro_EventManager
-    [SerializeField] private TextMeshProUGUI text;
+    //[SerializeField] private TextMeshProUGUI text;
+    public Text text2;
+    public FontStyle font;
 
     public void Awake()
     {
-         text = GetComponentInChildren<TextMeshProUGUI>();
+        text2 = GetComponentInChildren<Text>();
+        //font = GetComponent<Text>();
     }
     // When highlighted 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        text.textStyle = TMP_Style.NormalStyle;
-        text.fontStyle = FontStyles.Bold;
+        //text2.textStyle = TMP_Style.NormalStyle;
+
+        text2.fontStyle = FontStyle.Bold;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        text.textStyle = TMP_Style.NormalStyle;
-        text.fontStyle = FontStyles.Normal;
+        
+        //text.textStyle = TMP_Style.NormalStyle;
+        //text.fontStyle = FontStyles.Normal;
+
+        text2.fontStyle = FontStyle.Normal;
+
     }
 
     /*// When selected.
