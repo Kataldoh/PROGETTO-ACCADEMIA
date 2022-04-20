96fcf7b66ec4a8658d0f57860df4c7e0a724cb38
Fragalax <79252569+Fragalax@users.noreply.github.com>
2022-04-20 10:36:48 +0200

    Text Update

--- Assets/Scripts/UIScripts/ButtonsExtraScript.cs
+++ Assets/Scripts/UIScripts/ButtonsExtraScript.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
-using TMPro;
+using UnityEngine.UI;
 
 public class ButtonsExtraScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler//, ISelectHandler
 {
     //TMPro_EventManager
-    [SerializeField] private TextMeshProUGUI text;
+    private Text text;
 
     public void Awake()
     {
-         text = GetComponentInChildren<TextMeshProUGUI>();
+         text = GetComponentInChildren<Text>();
     }
     // When highlighted 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        text.textStyle = TMP_Style.NormalStyle;
-        text.fontStyle = FontStyles.Bold;
+        text.fontStyle = FontStyle.Bold;
     }
 
+    //When not highlighted from cursor
     public void OnPointerExit(PointerEventData eventData)
     {
-        text.textStyle = TMP_Style.NormalStyle;
-        text.fontStyle = FontStyles.Normal;
+        text.fontStyle = FontStyle.Normal;
     }
 
     /*// When selected.
