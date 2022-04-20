9c1a745bb868d433f649ae83bc3ff7ef407fc36a
FedericoBondi <federicobondi95@gmail.com>
2022-04-20 17:06:36 +0200

    Oof

--- bonus.tsx
+++ bonus.tsx
@@ -1,28 +1,31 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Nemici/bonus" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+<tileset version="1.8" tiledversion="1.8.4" name="Nemici/bonus" tilewidth="16" tileheight="16" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass1.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass1.png"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass3.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass3.png"/>
  </tile>
  <tile id="2">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass4.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass4.png"/>
  </tile>
  <tile id="3">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass7.png"/>
+  <properties>
+   <property name="Camera Stretta" value="ciao"/>
+  </properties>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass7.png"/>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass8.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass8.png"/>
  </tile>
  <tile id="5">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/grass9.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/grass9.png"/>
  </tile>
  <tile id="6">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/water1.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/water1.png"/>
  </tile>
  <tile id="7">
-  <image width="16" height="16" source="../../../Desktop/Game Design/Platformer - desert/Tileset/water2.png"/>
+  <image width="16" height="16" source="Assets/Tiled/Platformer - desert/Tileset/water2.png"/>
  </tile>
 </tileset>
