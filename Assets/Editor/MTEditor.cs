20b6ca4971ac2196803958575f2a950eb88a3e32
fabrizio@radicadesign.com <!Lemlo231>
2022-04-27 12:06:28 +0200

    editor 0.1

---
+++ Assets/Editor/MTEditor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MTEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+    }
+}
---
+++ Assets/TiledReader/MTTIleEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class MTTIleEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static MTTIleEditor instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
