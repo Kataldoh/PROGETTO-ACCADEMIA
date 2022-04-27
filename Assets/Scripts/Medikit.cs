fd635954d1c20ac83f8c7f8949cd7c6cf0fdb078
RiccardoSantochi <79253340+RiccardoSantochi@users.noreply.github.com>
2022-04-27 16:45:39 +0200

    Medikit

---
+++ Assets/Scripts/Medikit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medikit: MonoBehaviour
+{
+
+    public int cura;
+    private void OntriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            //richiamo il metodo TakeDamage
+            GameController.instance.TakeDamage(-cura);
+            Destroy(collision.gameObject);
+        }
+    }
+}
