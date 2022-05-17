b55fd56a755dd3cbeb774df84ac1339999b84634
EliasLevin <79252531+EliasLevin@users.noreply.github.com>
2022-05-17 17:27:29 +0200

    Update nemici + Bug che non mi so spiegare

--- Assets/Scripts/AI/EnemyCrawler_V2.cs
+++ Assets/Scripts/AI/EnemyCrawler_V2.cs
@@ -26,22 +26,30 @@ public class EnemyCrawler_V2 : EnemyScript
     public override void Patrol()
     {
         RaycastHit hit, hit2, hit3;
-        bool GroundCast = Physics.Raycast(groundRaycast[0].position, -transform.up, out hit, 0.5f, layer);
-        bool GroundCast2 = Physics.Raycast(groundRaycast[1].position, -transform.up, out hit2, 0.5f, layer);
-        bool WallCast = Physics.Raycast(frontRaycast.position, transform.right, out hit3, 0.2f, layer);
+        bool GroundCast = Physics.Raycast(groundRaycast[0].position, -transform.up, out hit, 0.2f, layer);
+        bool GroundCast2 = Physics.Raycast(groundRaycast[1].position, -transform.up, out hit2, 0.2f, layer);
+        bool WallCast = Physics.Raycast(frontRaycast.position, transform.right, out hit3, 0.15f, layer);
         if (!GroundCast && !GroundCast2)
         {
             if (!hasTurned)
             {
                 transform.Rotate(0, 0, -90);
+                hasTurned = true;
             }
-            hasTurned = true;
+
         }
         else
+        {
+            if (WallCast)
+            {
+                transform.Rotate(0, 0, 90);
+            }
             hasTurned = false;
+        }
+            
 
-        if (WallCast)
-            transform.Rotate(0, 0, 90);
+        
+
 
 
         controller.Move(transform.right * edata.force);
@@ -51,6 +59,15 @@ public class EnemyCrawler_V2 : EnemyScript
     {
         
     }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Nemico")
+        {
+            transform.Rotate(0, 180, 0);
+        }
+    }
+
     public override void StatelessChecks()
     {
         //no checks needed
