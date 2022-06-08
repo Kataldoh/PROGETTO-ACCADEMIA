6a7d0e9808f14a3caa809833f09a994462007a9d
EliasLevin <pavanelloelia1@gmail.com>
2022-06-08 12:13:26 +0200

    Walljump mezzo fix

--- Assets/Scripts/AI/EnemyScript.cs
+++ Assets/Scripts/AI/EnemyScript.cs
@@ -168,13 +168,14 @@ public class EnemyScript : MonoBehaviour
         isDead = true;
         SoundManager.PlaySound(SoundManager.Sound.EnemyDie);
         anim.SetBool("death", true);
+
         Vector3 healthSpawnPos;
         if (GameController.instance.CurrentHealth < 100)
         {
             healthSpawnPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
             Instantiate(spawnOnDeath, healthSpawnPos, spawnOnDeath.transform.rotation);
         }
-            
+        
         collider.enabled = false;
         if (controller != null)
         {
